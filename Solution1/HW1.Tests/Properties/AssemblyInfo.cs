467ff09ca115fd246ddee5f034cdf47b6790cb0e
Alexey <maneg1@outlook.com>
2020-07-21 10:34:46 +0300

    commit

---
+++ Solution1/HW1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HW1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HW1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0391164e-c7a2-4cf6-adde-24087ef2683e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Solution1/HW1/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
