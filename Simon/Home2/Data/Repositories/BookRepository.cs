2fbc195e7005c8612892ffc4892e9aaabd172463
simon <semenalesa113498@gmail.com>
2020-07-29 21:57:44 +0300

    home2

---
+++ Simon/Home2/Data/Repositories/BookRepository.cs
@@ -0,0 +1,23 @@
+﻿using DomainLogics.Models;
+using DomainLogics.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    class BookRepository :BaseRepository, IBookRepository
+    {
+        public List<Book> AllBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> GetMostPopular()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
