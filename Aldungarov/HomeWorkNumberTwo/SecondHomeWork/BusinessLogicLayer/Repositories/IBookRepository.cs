2c2b7895e151a5ba855d5c95b85406bf8acf2735
Aldungarov <kinanet@bigmir.net>
2020-07-30 11:59:08 +0300

    Added second homework.

---
+++ Aldungarov/HomeWorkNumberTwo/SecondHomeWork/BusinessLogicLayer/Repositories/IBookRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessLogicLayer.Repositories
+{
+    public interface IBookRepository : IBaseRepository
+    {
+        Book GetBook();
+        List<Book> GetAllBooks();
+    }
+}
