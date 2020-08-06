db77b7e243bc53382094fa4d0003b1b9ef129bf2
YuriPetruchenia <yourockpet@gmail.com>
2020-08-06 20:14:07 +0300

    Add my HomeWork

---
+++ Petruchenia/HomeWorkNumTwo/ClassLibrary2/Repositories/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2.Repositories
+{
+    public class BaseRepository : IBaseRepository
+    {
+        // CRUD 
+        public int Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Petruchenia/HomeWorkNumTwo/ClassLibrary2/Repositories/Interfaces/IBookRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibrary2.Repositories
+{
+    public interface IBookRepository : IBaseRepository
+    {
+        BookData Show5MostPopular();
+        BookData ShowAllBooks();
+        BookData ShowAllOrders();
+        BookData ShowAllOrdersFromToday();
+    }
+}
