464ee14edf278d3d022be07a46f1553cfe081cab
Viktar Radzin <viktarradzin@outlook.com>
2020-08-30 16:10:26 +0300

    Add HomeWorkNumberThree

---
+++ Radzin Viktar/HomeWorkNumberThree/Home.Users/Home.Users/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Home.Users.MvcApplication" Language="C#" %>
---
+++ Radzin Viktar/HomeWorkNumberThree/Home.Users/Home.Users/Models/City.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Home.Users.Models
+{
+    public class City
+    {
+        [Display(Name = "City")]
+        public string Name { get; set; }
+
+        public City() { }
+
+        public City(string Name) 
+        {
+            this.Name = Name;
+        }
+    }
+}
