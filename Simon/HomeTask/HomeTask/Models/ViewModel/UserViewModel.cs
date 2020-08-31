9de10f7a4c4d1cd84f689e6679a72b658afba0ef
simon <semenalesa113498@gmail.com>
2020-08-31 17:04:11 +0300

    task3

---
+++ Simon/HomeTask/HomeTask/Models/ViewModel/UserViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeTask.Models.ViewModel
+{
+    public enum Title
+    {
+        Mr,
+        Dr
+    }
+
+    public enum Country
+    {
+        RF,
+        Rb
+    }
+
+    public enum City
+    {
+        Minsk,
+        Homel
+    }
+
+    public class UserViewModel
+    {   
+            public int Id { get; set; }
+
+            public string FullName { get; set; }
+
+            public string Phone { get; set; }
+
+            public string Email { get; set; }
+             
+            public Title Title { get; set; }
+
+            public Country Country { get; set; }
+
+            public City City { get; set; }
+
+            public string Comments { get; set; }
+
+        
+    }
+}
---
+++ Simon/HomeTask/HomeTask/Repository/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using HomeTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask.Repository
+{
+   public interface IUserRepository
+    {
+        List<User> GetAllUsers();
+
+        List<User> Add(User user);
+
+        void Delete(int id);
+
+        List<User> Edit(User user);
+    }
+}
