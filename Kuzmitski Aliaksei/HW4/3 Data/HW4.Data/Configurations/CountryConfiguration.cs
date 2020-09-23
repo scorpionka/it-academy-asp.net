4d77de1123156c338aa47926d3883131c1d4939f
Алексей Кузьмицкий <mgpp@tut.by>
2020-09-23 18:52:20 +0300

    ready for migration

--- Kuzmitski Aliaksei/HW4/3 Data/HW4.Data/Configurations/CountryConfiguration.cs
+++ Kuzmitski Aliaksei/HW4/3 Data/HW4.Data/Configurations/CountryConfiguration.cs
@@ -1,6 +1,18 @@
-﻿namespace HW4.Data.Configurations
+﻿using HW4.Domain.Models;
+using System.Data.Entity.ModelConfiguration;
+
+namespace HW4.Data.Configurations
 {
-    public class CountryConfiguration
+    public class CountryConfiguration : EntityTypeConfiguration<Country>
     {
+        public CountryConfiguration()
+        {
+            ToTable("Countries");
+
+            HasKey(x => x.Id);
+
+            Property(x => x.Name).IsRequired().HasMaxLength(30);
+            HasIndex(x => x.Name).IsUnique(true);
+        }
     }
 }
