2d3ede6f9ee16a13b8aa72b88f4dbbee6315fd6a
Алексей Кузьмицкий <mgpp@tut.by>
2020-09-23 19:02:59 +0300

    ending successfully migration

---
+++ Kuzmitski Aliaksei/HW4/3 Data/HW4.Data/Migrations/202009231555354_UsersDatabaseHW4.cs
@@ -0,0 +1,71 @@
+﻿namespace HW4.Data.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class UsersDatabaseHW4 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cities",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Name = c.String(nullable: false, maxLength: 25),
+                    CountryId = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Countries", t => t.CountryId)
+                .Index(t => t.CountryId);
+
+            CreateTable(
+                "dbo.Countries",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Name = c.String(nullable: false, maxLength: 30),
+                })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true);
+
+            CreateTable(
+                "dbo.Users",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    FirstName = c.String(name: "First Name", nullable: false, maxLength: 20),
+                    LastName = c.String(name: "Last Name", nullable: false, maxLength: 30),
+                    Title = c.Int(nullable: false),
+                    Phone = c.String(nullable: false, maxLength: 15),
+                    Email = c.String(nullable: false, maxLength: 30),
+                    Comments = c.String(maxLength: 300),
+                    CityId = c.Int(nullable: false),
+                    CountryId = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Cities", t => t.CityId)
+                .ForeignKey("dbo.Countries", t => t.CountryId)
+                .Index(t => t.Phone, unique: true)
+                .Index(t => t.Email, unique: true)
+                .Index(t => t.CityId)
+                .Index(t => t.CountryId);
+
+        }
+
+        public override void Down()
+        {
+            DropForeignKey("dbo.Cities", "CountryId", "dbo.Countries");
+            DropForeignKey("dbo.Users", "CountryId", "dbo.Countries");
+            DropForeignKey("dbo.Users", "CityId", "dbo.Cities");
+            DropIndex("dbo.Users", new[] { "CountryId" });
+            DropIndex("dbo.Users", new[] { "CityId" });
+            DropIndex("dbo.Users", new[] { "Email" });
+            DropIndex("dbo.Users", new[] { "Phone" });
+            DropIndex("dbo.Countries", new[] { "Name" });
+            DropIndex("dbo.Cities", new[] { "CountryId" });
+            DropTable("dbo.Users");
+            DropTable("dbo.Countries");
+            DropTable("dbo.Cities");
+        }
+    }
+}
---
+++ Kuzmitski Aliaksei/HW4/3 Data/HW4.Data/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace HW4.Data.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HW4.Data.Context.DatabaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(HW4.Data.Context.DatabaseContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
